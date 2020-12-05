51cb0bc759ef54c980b0d672ac297e598e761d71
Patricia Pereira <patriciagpereira89@gmail.com>
2020-12-05 16:35:55 -0300

    Incluido método potencia_quadrados

--- src/main/java/com/mycompany/calculadorasimples2/CalculadoraSimples.java
+++ src/main/java/com/mycompany/calculadorasimples2/CalculadoraSimples.java
@@ -248,10 +248,10 @@ public class CalculadoraSimples {
         inverte(v);//chama o método
         return v;
     }
-    
-    public double potencia_quadrados(double n1, int n){
+
+    public double potencia_quadrados(double n1, int n) {
         int n2 = soma_quadrados(n);
-        double r = potencia(n1,n2);
+        double r = potencia(n1, n2);
         return r;
     }
 
